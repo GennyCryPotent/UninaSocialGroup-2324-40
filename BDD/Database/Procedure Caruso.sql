f7682e468d87e66fe40c9eb5ad631a315b5b235f
GennyCryPotent <153099957+GennyCryPotent@users.noreply.github.com>
2024-07-06 01:40:25 +0200

    Update Gennaro.sql and Caruso.sql

--- BDD/Database/Procedure Caruso.sql
+++ BDD/Database/Procedure Caruso.sql
@@ -1,6 +1,3 @@
---Buon Divertimento
-
-
 -- CREA PARTECIPANO
 create or replace NONEDITIONABLE PROCEDURE Crea_Partecipano (P_Nome_Utente IN PARTECIPANO.FK_NOME_UTENTE%TYPE, P_Id_Gruppo IN PARTECIPANO.FK_Id_Gruppo%TYPE)
 AS
@@ -8,6 +5,7 @@ AS
 BEGIN
     INSERT INTO Partecipano VALUES (P_Nome_Utente, P_Id_Gruppo);
 END Crea_Partecipano;
+/
 
 -- CREA REGOLANO
 
@@ -17,7 +15,7 @@ AS
 BEGIN
     INSERT INTO Regolano VALUES (P_Nome_Utente, P_Id_Gruppo);
 END Crea_Regolano;
-
+/
 
 -- CREA LIKES
 create or replace NONEDITIONABLE PROCEDURE Crea_Likes (P_Nome_Utente IN Likes.FK_NOME_UTENTE%TYPE, P_Id_Contenuto IN Likes.FK_Id_Contenuto%TYPE)
@@ -26,3 +24,13 @@ AS
 BEGIN
     INSERT INTO Likes VALUES (P_Nome_Utente, P_Id_Contenuto);
 END Crea_Likes;
+
+-- CREA POSSIEDONO
+
+create or replace NONEDITIONABLE PROCEDURE Crea_Possiedono (P_Id_Gruppo IN Possiedono.FK_Id_Gruppo%TYPE, P_FK_Parola IN Possiedono.FK_Parola%TYPE )
+AS
+
+BEGIN
+    INSERT INTO Possiedono VALUES (P_Id_Gruppo, P_Parola);
+END Crea_Possiedono;
+/
