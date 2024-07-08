a60c36651ef1131888017eeb49706b069fffe5e1
Antonio Caruso <giggi203@gmail.com>
2024-07-09 01:27:57 +0200

    Aggiornamento Procedure e Altro
    Aggiunta delle procedure:
    -Crea_Notifica_Gruppo : auto-esplicativo
    -Crea_Notifica_Contenuto ; auto-esplicativo
    -Mostra_Notifica : Mostra le notifiche di un utente e le visualizza (sfruttando le procedure che seguono);
    -Visualizzato_Notifica_Contenuto : visualizza una notifica di un  contenuto di un utente;
    -Visualizzato_Notifica_Gruppo :
    visualizza una notifica di un gruppo a cui un utente partecipa;
    
    Aggiunta le eliminazioni delle nuove procedure in "EliminaDB.txt"
    
    Modifica e aggiunta di alcuni comandi utili in "Popolamento.sql"

--- BDD/Database/Popolamento.sql
+++ BDD/Database/Popolamento.sql
@@ -1,20 +1,28 @@
 --POPOLAZIONE DB
 
 --Popolamento Profili
-Insert into Profili (Nome_Utente, Password, Nome, Cognome, Genere, Data_Nascita)Values ('Genny03cry', 'Database03', 'Gennaro', 'De Luca', 'M', TO_DATE('04-11-2003', 'DD-MM-YYYY'));
-Insert into Profili (Nome_Utente, Password, Nome, Cognome, Genere, Data_Nascita) Values ('Gabbo', 'SonoIo02', 'Gabriele', 'Cifuni', 'F', TO_DATE('21-4-2002', 'DD-MM-YYYY'));
-Insert into Profili (Nome_Utente, Password, Nome, Cognome, Genere, Data_Nascita) Values ('DarkNine', 'Pagliaccio', 'Simone', 'Francesco', 'F', TO_DATE('21-11-2003', 'DD-MM-YYYY'));
-Insert into Profili (Nome_Utente, Password, Nome, Cognome, Genere, Data_Nascita) Values ('errore31', 'Sbagliato03', 'Antonio', 'Caruso', 'M', TO_DATE('27-05-2003', 'DD-MM-YYYY'));
+CALL CREA_UTENTE('Genny03cry', 'Database03', 'Gennaro', 'De Luca', 'M', TO_DATE('04-11-2003', 'DD-MM-YYYY'));
+CALL CREA_UTENTE('Gabbo', 'SonoIo02', 'Gabriele', 'Cifuni', 'F', TO_DATE('21-4-2002', 'DD-MM-YYYY'));
+CALL CREA_UTENTE('DarkNine', 'Pagliaccio', 'Simone', 'Francesco', 'F', TO_DATE('21-11-2003', 'DD-MM-YYYY'));
+CALL CREA_UTENTE('errore31', 'Sbagliato03', 'Antonio', 'Caruso', 'M', TO_DATE('27-05-2003', 'DD-MM-YYYY'));
+
+
 
 --Popolamento Gruppi
-Insert into Gruppi (Nome, Descrizione, fk_Nome_Utente) Values ('Fantacalcio', 'Ciao', 'Genny03cry');
-Insert into Gruppi (Nome, Descrizione, fk_Nome_Utente) Values ('SSC_Napoli_Ultras', 'Solo fan del napoli', 'Genny03cry');
+CALL CREA_GRUPPO ('Fantacalcio', 'Ciao', 'Genny03cry');
+CALL CREA_GRUPPO ('SSC_Napoli_Ultras', 'Solo fan del napoli', 'Genny03cry');
 
 --Popolamneto NOTIFICHE_RICHIESTE_ESITI
 -- INSERT INTO Notifiche_Richieste_Esiti (Testo, FK_Id_Gruppo, FK_Nome_Utente) VALUES (FK_Nome_Utente ||' Ha inviato richiesta al gruppo: ' || (SELECT Nome FROM Gruppi WHERE FK_Id_Gruppo = Id_Gruppo), 1, 'Gabbo');
 CALL CREA_NOTIFICA_RICHIESTA_ESITO (1, 'Gabbo');
 CALL CREA_NOTIFICA_RICHIESTA_ESITO (1, 'DarkNine');
 CALL CREA_NOTIFICA_RICHIESTA_ESITO (1, 'errore31');
+
+
+--Popolamneto NOTIFICHE_RICHIESTE_ESITI (ACCETTAZIONI)
+INSERT INTO Notifiche_Richieste_Esiti(Testo, Esitato,FK_Id_Gruppo, FK_Nome_Utente) VALUES ('Gabbo è stato aggiunto al gruppo: Fantacalcio', '3', 1, 'Gabbo');      
+INSERT INTO Notifiche_Richieste_Esiti(Testo, Esitato,FK_Id_Gruppo, FK_Nome_Utente) VALUES ('errore31 è stato aggiunto al gruppo: Fantacalcio', '3', 1, 'errore31');      
+
 
 --Popolamento Contenuti
 -- INSERT INTO Contenuti (Testo, fk_id_gruppo, fk_nome_utente) VALUES ('Pako ha vinto il fanta', 1, 'Gabbo');
@@ -30,10 +38,10 @@ CALL CREA_CONTENUTO(NULL, 'Buongiorno è troppo forte',1,'DarkNine');
 -- INSERT INTO Partecipano VALUES ('DarkNine', 1);
 
 --Popolamento Like
-CALL Create_Like ('DarkNine', 2);
-CALL Create_Like ('Gabbo', 2);
-CALL Create_Like ('DarkNine', 1);
-CALL Create_Like ('Gabbo', 1); 
+CALL Crea_Like ('DarkNine', 2);
+CALL Crea_Like ('Gabbo', 2);
+CALL Crea_Like ('DarkNine', 1);
+CALL Crea_Like ('Gabbo', 1); 
 
 
 
@@ -47,7 +55,10 @@ CALL Create_Like ('Gabbo', 1);
 
 
 --Mostra Richieste
-CALL MOSTRA_RICHIESTE('Genny03cry');
+CALL MOSTRA_RICHIESTA('Genny03cry');
+
+
+CALL MOSTRA_ARCHIVIATA('Genny03cry');
 
 --Update Accettazione
 UPDATE notifiche_richieste_esiti SET Esitato = '1' WHERE id_notifica_re = 1;
