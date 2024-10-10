96a7bed83b3bf42a2fcf090bb9f0c774821b53df
GabrieleC14 <153102633+GabrieleC14@users.noreply.github.com>
2024-10-10 18:48:19 +0200

    .

--- OO/UninaSocialGroup/src/main/java/UninaSocialGroup/Gestione_Finestre.java
+++ OO/UninaSocialGroup/src/main/java/UninaSocialGroup/Gestione_Finestre.java
@@ -24,11 +24,11 @@ public class Gestione_Finestre {
 	//public static DB_Connection DB = new DB_Connection("jdbc:oracle:thin:@localhost:1521:ORCL", "system", "Unina@03"); //portatile genny
 	//public static DB_Connection DB = new DB_Connection("jdbc:oracle:thin:@Gennaro.homenet.telecomitalia.it:1521:xe", "system", "Database@03"); //Fisso Genny
 
-	public static DB_Connection DB = new DB_Connection("jdbc:oracle:thin:@DESKTOP-MLJV8GK:1521:xe", "system", "Caruso"); //Caruso
+	//public static DB_Connection DB = new DB_Connection("jdbc:oracle:thin:@DESKTOP-MLJV8GK:1521:xe", "system", "Caruso"); //Caruso
 
 	//public static DB_Connection DB = new DB_Connection("jdbc:oracle:thin:@errore31:1521:xe", "SYSTEM", "Caruso"); //Caruso Portatile
 	
-	//public static DB_Connection DB = new DB_Connection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Database@03"); //Gabbo
+	public static DB_Connection DB = new DB_Connection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Database@03"); //Gabbo
 
 	static {
 		try {
@@ -50,7 +50,7 @@ public class Gestione_Finestre {
 			public void run() {
 				try {
 					DB.connect();
-					Home_GUI frame = new Home_GUI("Genny03cry");
+					Home_GUI frame = new Home_GUI("Gabbo");
 
 					//Login_GUI frame = new Login_GUI();
 					frame.setVisible(true);
