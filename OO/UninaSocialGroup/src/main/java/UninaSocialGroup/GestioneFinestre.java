5562ed8256eb82e7af9580ee58a91ca569295cf7
GennyCryPotent <153099957+GennyCryPotent@users.noreply.github.com>
2024-10-14 15:38:06 +0200

    Aggiornamento GestioneFinestre

--- OO/UninaSocialGroup/src/main/java/UninaSocialGroup/GestioneFinestre.java
+++ OO/UninaSocialGroup/src/main/java/UninaSocialGroup/GestioneFinestre.java
@@ -8,26 +8,26 @@ import com.formdev.flatlaf.FlatLightLaf;
 
 public class GestioneFinestre {
 	
-	private HomeGUI Accesso;
-	private GruppiGUI Gruppo;
-	private NotificheGUI Notifica;  
-	private OperazioniPostCommentoGUI Elimina; 
-	private ReportStatisticoGUI Report;
-	private InfoPost_GUI InfoPost;
-	private Registrazione_GUI Registrazione;
-	private LoginGUI Login;
-	private RicercaGUI Ricerca;
-	private OperazioniPartecipanteGUI EliminaP;
-	private CreaGruppoGUI CreaG;
+	private HomeGUI home;
+	private GruppiGUI gruppo;
+	private NotificheGUI notifica;  
+	private OperazioniPostCommentoGUI elimina; 
+	private ReportStatisticoGUI report;
+	private InfoPost_GUI infoPost;
+	private Registrazione_GUI registrazione;
+	private LoginGUI login;
+	private RicercaGUI ricerca;
+	private OperazioniPartecipanteGUI eliminaPartecpiante;
+	private CreaGruppoGUI creaGruppo;
 	
-	public static DBConnection DB = new DBConnection("jdbc:oracle:thin:@localhost:1521:ORCL", "system", "Unina@03"); //portatile genny
+	//public static DBConnection DB = new DBConnection("jdbc:oracle:thin:@localhost:1521:ORCL", "system", "Unina@03"); //portatile genny
 	//public static DB_Connection DB = new DBConnection("jdbc:oracle:thin:@Gennaro.homenet.telecomitalia.it:1521:xe", "system", "Database@03"); //Fisso Genny
 
 	//public static DBConnection DB = new DBConnection("jdbc:oracle:thin:@DESKTOP-MLJV8GK:1521:xe", "system", "Caruso"); //Caruso
 
 	//public static DB_Connection DB = new DBConnection("jdbc:oracle:thin:@errore31:1521:xe", "SYSTEM", "Caruso"); //Caruso Portatile
 	
-	//public static DB_Connection DB = new DBConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Database@03"); //Gabbo
+	public static DBConnection DB = new DBConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Database@03"); //Gabbo
 
 	static {
 		try {
@@ -58,71 +58,71 @@ public class GestioneFinestre {
 	}
 	
 	  public void MostraGruppi(String nomeUtente, String nomeGruppo) {
-		  Gruppo = new GruppiGUI(nomeUtente, nomeGruppo);
-		  Gruppo.setVisible(true);
+		  gruppo = new GruppiGUI(nomeUtente, nomeGruppo);
+		  gruppo.setVisible(true);
 	  
 	  }
 	
 	  public void AccessoHome(String nomeUtente) {
-		  Accesso = new HomeGUI(nomeUtente); 
-		  Accesso.setVisible(true);
+		  home = new HomeGUI(nomeUtente); 
+		  home.setVisible(true);
 		  
 	  }
 	  
 	  public void EliminaContenuto(String nomeGruppo, String nomeUtente, GruppiGUI gruppiView) {
-		  Elimina = new OperazioniPostCommentoGUI(nomeGruppo, nomeUtente, gruppiView); 
-		  Elimina.setVisible(true);
+		  elimina = new OperazioniPostCommentoGUI(nomeGruppo, nomeUtente, gruppiView); 
+		  elimina.setVisible(true);
 		
 	  }
 	  
 	  public void EliminaCommento(String nomeGruppo, String nomeUtente, int Id_Contenuto, int checkSchermata) {
-		  Elimina = new OperazioniPostCommentoGUI(nomeGruppo, nomeUtente, Id_Contenuto, checkSchermata); 
-		  Elimina.setVisible(true);
+		  elimina = new OperazioniPostCommentoGUI(nomeGruppo, nomeUtente, Id_Contenuto, checkSchermata); 
+		  elimina.setVisible(true);
 		
 	  }
 	  
 	  public void InfoPost(int Id_Contenuto, String nomeUtente, String nomeGruppo, int check ) {
-		  InfoPost = new InfoPost_GUI(Id_Contenuto,nomeUtente, nomeGruppo, check); 
-		  InfoPost.setVisible(true);
+		  infoPost = new InfoPost_GUI(Id_Contenuto,nomeUtente, nomeGruppo, check); 
+		  infoPost.setVisible(true);
 		
 	  }
 
 	  
 	  public void MostraNotifiche(String nomeUtente) {
-		  Notifica = new NotificheGUI(nomeUtente);
-		  Notifica.setVisible(true);
+		  notifica = new NotificheGUI(nomeUtente);
+		  notifica.setVisible(true);
 		  
 	  }
 	  
 	  public void ReportS(String nomeUtente) {
-		  Report = new ReportStatisticoGUI(nomeUtente);
-		  Report.setVisible(true);
+		  report = new ReportStatisticoGUI(nomeUtente);
+		  report.setVisible(true);
 	  }
 	  
 	  public void MostraRegistrazione() {
-		  Registrazione = new Registrazione_GUI();
-		  Registrazione.setVisible(true);
+		  registrazione = new Registrazione_GUI();
+		  registrazione.setVisible(true);
 	  }
 	  
 	  public void MostraLogin() {
-		  Login = new LoginGUI();
-		  Login.setVisible(true);
+		  login = new LoginGUI();
+		  login.setVisible(true);
 		  
 	  }
 	  
 	  public void MostraRicerca(String nomeUtente, HomeGUI homeView) {
-		  Ricerca = new RicercaGUI(nomeUtente, homeView);
-		  Ricerca.setVisible(true);
+		  ricerca = new RicercaGUI(nomeUtente, homeView);
+		  ricerca.setVisible(true);
 	  }
 	  
 	  public void EliminaPartecipante(String nomeUtente, String nomeGruppo, String Ruolo, int checkOp) {
-		  EliminaP = new OperazioniPartecipanteGUI(nomeUtente, nomeGruppo, Ruolo, checkOp);
-		  EliminaP.setVisible(true);
+		  eliminaPartecpiante = new OperazioniPartecipanteGUI(nomeUtente, nomeGruppo, Ruolo, checkOp);
+		  eliminaPartecpiante.setVisible(true);
 	  }
 	  
 	  public void CreaGruppo(String nomeUtente, HomeGUI home) {
-		  CreaG = new CreaGruppoGUI(nomeUtente, home);
-		  CreaG.setVisible(true);
+		  creaGruppo = new CreaGruppoGUI(nomeUtente, home);
+		  creaGruppo.setVisible(true);
 	  }
 	  
 }
