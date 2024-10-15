93204fa56c1d79857f81fcf49016e39f07c1a40e
GennyCryPotent <153099957+GennyCryPotent@users.noreply.github.com>
2024-10-15 19:37:28 +0200

    Update GestioneFinestre.java

--- OO/UninaSocialGroup/src/main/java/UninaSocialGroup/GestioneFinestre.java
+++ OO/UninaSocialGroup/src/main/java/UninaSocialGroup/GestioneFinestre.java
@@ -22,15 +22,20 @@ public class GestioneFinestre {
 	private OperazioniPartecipanteGUI eliminaPartecpiante;
 	private CreaGruppoGUI creaGruppo;
 	
-	//public static DBConnection DB = new DBConnection("jdbc:oracle:thin:@localhost:1521:ORCL", "system", "Unina@03"); //portatile genny
+	public static DBConnection DB = new DBConnection("jdbc:oracle:thin:@localhost:1521:ORCL", "system", "Unina@03"); //portatile genny
+	
+	/*
+	 * 
+	 * Collegamenti con altri dispositivi
 	//public static DB_Connection DB = new DBConnection("jdbc:oracle:thin:@Gennaro.homenet.telecomitalia.it:1521:xe", "system", "Database@03"); //Fisso Genny
 
 	//public static DBConnection DB = new DBConnection("jdbc:oracle:thin:@DESKTOP-MLJV8GK:1521:xe", "system", "Caruso"); //Caruso
 
 	//public static DB_Connection DB = new DBConnection("jdbc:oracle:thin:@errore31:1521:xe", "SYSTEM", "Caruso"); //Caruso Portatile
 	
-	public static DBConnection DB = new DBConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Database@03"); //Gabbo
-
+	//public static DBConnection DB = new DBConnection("jdbc:oracle:thin:@localhost:1521:xe", "system", "Database@03"); //Gabbo
+	*/
+	
 	static {
 		try {
 			UIManager.setLookAndFeel(new FlatLightLaf());
